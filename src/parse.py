c9596121b924f3b72baf36e5d07498dbb4f571a8
Ryan Kelly <ryan@ryankellypercussion.com>
2020-05-20 11:28:31 -0400

    Rewrite var name

--- src/parse.py
+++ src/parse.py
@@ -3,9 +3,9 @@ import stanza
 stanza.download('en')
 en_nlp = stanza.Pipeline('en')
 
-sentence = en_nlp("If Socrates is a human, he is mortal. Socrates is mortal if he is a human. Socrates is mortal or Socrates is immortal. Socrates is a human, and Socrates is not a dancer. Therefore, Socrates is mortal.")
+doc = en_nlp("If Socrates is a human, he is mortal. Socrates is mortal if he is a human. Socrates is mortal or Socrates is immortal. Socrates is a human, and Socrates is not a dancer. Therefore, Socrates is mortal.")
 
-for i, sent in enumerate(sentence.sentences):
+for i, sent in enumerate(doc.sentences):
     print("[Sentence {}]".format(i+1))
     for word in sent.words:
         print("{:12s}\t{:12s}\t{:6s}\t{:d}\t{:12s}".format(\
